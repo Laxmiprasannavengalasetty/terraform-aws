e5ff6d1bd2911ea3af492936bbd23fbf36290f91
Laxmiprasanna <v.l.prasanna55@gmail.com>
2024-09-12 09:40:16 +1000

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow-ssh-terraform" {
     name = "allow-ssh-tf"
     description= " allow port no. 22 for ssh access"
-
+   
 
 # outgoing traffic usally we allow everything in egress not require any permission to go out
 egress {
@@ -29,6 +29,8 @@ tags = {
 resource "aws_instance" "terraform" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+
 
   tags = {
     Name = "terraform"

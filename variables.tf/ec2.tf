07892faff8490edf4f4cbf995feac71f0409b00f
Laxmiprasanna <v.l.prasanna55@gmail.com>
2024-09-12 09:10:57 +1000

    terraform

--- variables.tf/ec2.tf
+++ variables.tf/ec2.tf
@@ -30,7 +30,5 @@ resource "aws_instance" "terraform" {
   ami = var.ami_id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.allow-ssh-terraform.id]
-  tags = {
-    Name = "terraform"
-  }
+  tags =var.tags
 }
--- variables.tf/variable.tf
+++ variables.tf/variable.tf
@@ -1,6 +1,6 @@
 variable "ami_id" {
   type    = string
-  default = ["ami-09c813fb71547fc4f"]
+  default = "ami-09c813fb71547fc4f"
   description = "This is the AMI ID of DevOps RHEL 9"
 }
 
@@ -10,18 +10,13 @@ variable "instance_type"{
    description = "This is type of instance type"   #optional
 
 }
- 
- variable "sg_id"{
-   type = list(string)
-
- }
 
  variable "tags"{
    type = map      #optional it takes as a map
    default =  {
      Name = "backend"
      Project = "expense"
-     Component = "dev"
+     Component = "DEV"
      terraform = "true"
    }
  }
@@ -47,10 +42,10 @@ variable "instance_type"{
  }
  variable "protocol"{
    default = "tcp"
-   type = number
+  
 
  }
-  }
+  
  variable "ingress_cidr_blocks"{
    default = ["0.0.0.0/0"]
    type = list(string)

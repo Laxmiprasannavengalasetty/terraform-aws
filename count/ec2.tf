26be2bf4c0a60383a473fe2a189f231687c824cd
Laxmiprasanna <v.l.prasanna55@gmail.com>
2024-09-12 11:50:28 +1000

    terraform count- length function

--- count/ec2.tf
+++ count/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "backend" {
-  count = 3
+  count = length([var.instance_names])   # either 3 or this length function can use
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow-ssh-terraform.id]
